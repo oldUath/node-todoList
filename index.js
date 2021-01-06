2db67063eea82d76d9614c4eef39db0530179d2a
oldUath <haiyangdong666@gmail.com>
2021-01-06 14:25:53 +0800

    1

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const program= require('commander');
+const { Command } = require('commander');
+const program = new Command();
 program
     .option('-x, --xxx', 'what the xxx')
 program
-    .command('add <taskName>')
+    .command('add')
     .description('add a task')
     .action((...args) => {
         const words = args.slice(0,-1).join(' ')
-        console.log(words);
+        console.log(args[args.length-1]);
+    });
+program
+    .command('clear')
+    .description('clear all task')
+    .action((...args) => {
+        console.log('this is clear');
     });
 
 program.parse(process.argv);
