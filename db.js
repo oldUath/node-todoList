1c59e241cab7d6b3557a139e68f8369652e00e16
oldUath <haiyangdong666@gmail.com>
2021-01-10 16:28:38 +0800

    优化封装的方法

--- db.js
+++ db.js
@@ -7,18 +7,15 @@ const db={
     read(path = dbPath){
         return new Promise((resolve,reject)=>{
             fs.readFile(path,{flag:'a+'},(error,data)=>{
-                if(error){
-                    console.log(error)
-                    reject(error)
-                }else{
-                    let list
-                    try{
-                        list = JSON.parse(data.toString())
-                    } catch(error){
-                        list = []
-                    }
-                    resolve(list)
+                if(error){return reject(error)}
+                let list
+                try{
+                    list = JSON.parse(data.toString())
+                } catch(error){
+                    list = []
                 }
+                resolve(list)
+
             })
         })
     },
@@ -27,13 +24,8 @@ const db={
       return new Promise((resolve,reject)=>{
         const string = JSON.stringify(list)
         fs.writeFile(path,string,(error)=>{
-          if(error){
-            console.log(error)
-            reject(error)
-
-          }else{
-            resolve()
-          }
+          if(error){ return reject(error) }
+          resolve()
         })
       })
     }
