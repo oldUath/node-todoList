e835c41d08740465f68c5c1ff34f31b22750c144
oldUath <haiyangdong666@gmail.com>
2021-01-11 10:31:56 +0800

    去掉多余的代码

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 //系统用户目录C:\Users\Uath
-const homedir = require('os').homedir();
-const p = require('path');
-const fs = require('fs');
 const db = require("./db.js");
-const dbPath = p.join(homedir,'todoList')
-
-
 
 module.exports.add= async  (title)=>{
     //读之前的任务 ，因为读取是异步操作所以使用await和async
-    const list = await db.read(dbPath);
+    const list = await db.read();
     //添加一个任务
     list.push({title,done:false})
     //存储到文件里
