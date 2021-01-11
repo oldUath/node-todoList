724263fb42eae9fc262b03b82cb50a296c731eb2
oldUath <haiyangdong666@gmail.com>
2021-01-11 11:01:29 +0800

    asyc会强制返回一个promis,所以在调用他的方法后要添加一then方法，void是取消then方法

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@ const api = require('./index.js')
 
 //当后面没有参数时，就会打印出所有信息
 if(process.argv.length===2){
-  api.showAll()
+  void api.showAll()
   return
 }
 
@@ -14,14 +14,13 @@ program
     .command('add <taskName>')
     .description('add a task')
     .action((title) => {
-        api.add(title)
-      console.log(process.argv)
+        api.add(title).then(()=>{console.log('添加成功')},()=>{console.log('添加失败')} )
     });
 program
     .command('clear')
     .description('clear all task')
     .action(() => {
-        api.clear()
+        api.clear().then(()=>{console.log('清除成功')},()=>{console.log('清除失败')} )
     });
 
 program.parse(process.argv);
